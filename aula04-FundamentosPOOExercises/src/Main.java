8130ee72b5b0b5b7df8aed01770f0aff75524ada
allansmithll <allanammancio2004@gmail.com>
2023-03-02 21:39:00 -0300

    Exercício 1 de Fundamentos da POO resolvido

---
+++ aula04-FundamentosPOOExercises/src/Main.java
@@ -0,0 +1,12 @@
+import Exercicio1.Retangulo;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Primeira questão
+        Retangulo r = new Retangulo(1, 2, 3);
+
+        System.out.println(r.calcularPerimetro() + "m²");
+
+    }
+}
