c02b1eacbc6060022f9bbb942a30033131ec775a
AllanSmithll <101681192+AllanSmithll@users.noreply.github.com>
2023-05-12 16:34:54 -0300

    Update Teste.java

--- aula11-Herancas/aula02-Animal&Exercicios/src/Teste.java
+++ aula11-Herancas/aula02-Animal&Exercicios/src/Teste.java
@@ -18,6 +18,34 @@ public class Teste {
 		System.out.println( bob.aplicarInjecao(fifi) ) ;
 		System.out.println( bob.aplicarInjecao(rex) );
 		System.out.println(amanda.aplicarInjecao(juju));
+		
+		// Teste com ArrayList<>
+		ArrayList<Animal> animais = new ArrayList<>();
+		animais.add(new Cachorro("rex",7));
+		animais.add( new Gato("fifi",3, 5) );
+		animais.add( new Cachorro("lessy", 4) );
+		animais.add( new Gato("nino", 6, 2) );
+		
+		for (Animal a : animais){
+			System.out.println( a );
+		}
+		
+		//exibir o animal mais pesado
+		double maxpeso = 0;
+		Animal maispesado = null;
+		for (Animal a : animais)
+			if(a.getPeso() >= maxpeso){
+				maxpeso = a.getPeso();
+				maispesado = a;
+		}
+		System.out.println("\nAnimal mais pesado: "+ maispesado ); //nome=rex, peso=7.0, som= au au
+		
+		int cont=0;
+		for (Animal a : animais){
+			if(a instanceof Gato)
+				cont++;
+		}
+		System.out.println( cont ); //2
 	}
 
 }
